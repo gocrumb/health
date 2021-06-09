99fbde03cf7b6375964dd7dc6992ddd725c7d774
Mahmud Ridwan <m@hjr265.me>
2021-06-09 17:05:26 +0600

    Add Period option to redis

--- redis/option.go
+++ redis/option.go
@@ -1,5 +1,9 @@
 package redis
 
+import (
+	"time"
+)
+
 type Option interface {
 	apply(*check)
 }
@@ -8,4 +12,10 @@ type OptionFunc func(*check)
 
 func (o OptionFunc) apply(c *check) {
 	o(c)
+}
+
+func Period(d time.Duration) Option {
+	return OptionFunc(func(c *check) {
+		c.period = d
+	})
 }
